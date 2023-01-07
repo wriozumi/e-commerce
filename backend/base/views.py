9e7f7cd24097c10f331cb964f9af070489aaf73c
wriozumi <tshatmanaliev@gmail.com>
2023-01-07 20:40:27 +0100

    18. feat: Serializing data

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,6 +4,9 @@ from .products import products
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .models import Product
+from .serializer import ProductSerializer
+
 # Create your views here.
 
 @api_view(['GET'])
@@ -18,14 +21,13 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products) 
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data) 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product: None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
 
-    return Response(product) 
+    return Response(serializer.data) 
